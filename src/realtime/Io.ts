0c3d4f1fd6f19b0dc34160e82492efb2d97d362c
mohamed nagah <mohamed.nagah95@gmail.com>
2022-03-06 21:54:00 +0200

    remove unwanted comments from io file

--- src/realtime/Io.ts
+++ src/realtime/Io.ts
@@ -1,22 +1,3 @@
 import { Server } from 'socket.io';
 
-// interface ServerToClientEvents {
-//   noArg: () => void;
-//   basicEmit: (a: number, b: string, c: Buffer) => void;
-//   withAck: (d: string, callback: (e: number) => void) => void;
-// }
-
-// interface ClientToServerEvents {
-//   hello: () => void;
-// }
-
-// interface InterServerEvents {
-//   ping: () => void;
-// }
-
-// interface SocketData {
-//   name: string;
-//   age: number;
-// }
-
 export const io = (app: any) => new Server(app);
